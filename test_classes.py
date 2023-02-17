66c98145cf2c440da2d85f56410081b1b47cfaf5
Adam Muenz <ajmuenz@hotmail.com>
2023-02-17 15:18:43 -0800

    test_classes.py updated

--- test_classes.py
+++ test_classes.py
@@ -1,8 +1,14 @@
+import pytest
+
 class Person:
     def __init__(self, name):
         self.name = name
+    
+    # This makes the == chekc if name is the same instead of the whole instance of the Person class
+    def __eq__(self, other):
+        return self.name == other.name
 
 def test_classes_compare():
     p1 = Person("Mindy")
-    p2 = Person("Daniel")
-    assert p1.name == p2.name
+    p2 = Person("Mindy")
+    assert p1 == p2
